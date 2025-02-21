af7cfdfa565038f7f35e543e76136ab0bae045f7
Rishabh6353 <rishabhoop@gmail.com>
2025-02-21 11:31:58 +0530

    Update user.js
    user schema

--- models/user.js
+++ models/user.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
 
-const UserSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-  },
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePicture: { type: String, default: "" },
+  role: { type: String, enum: ["user", "admin", "trusted_user"], default: "user" },
+  points: { type: Number, default: 0 },
+  logsVerified: { type: Number, default: 0 },
+  reportsReceived: { type: Number, default: 0 },
+  rewardsClaimed: [{ 
+    rewardId: { type: mongoose.Schema.Types.ObjectId, ref: "Reward" },
+  }],
 });
 
 UserSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema);
