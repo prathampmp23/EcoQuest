735926752997cb984371adf2eb3278ad6aa6bed4
Rishabh6353 <rishabhoop@gmail.com>
2025-02-21 11:33:13 +0530

    Create reward.js
    reward schema

---
+++ models/reward.js
@@ -0,0 +1,8 @@
+const rewardSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String },
+    pointsRequired: { type: Number, required: true },
+    createdAt: { type: Date, default: Date.now }
+  });
+  
+  module.exports = mongoose.model("Reward", rewardSchema);
